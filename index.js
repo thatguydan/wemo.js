dd3fd100f31b6960e61700eca139c86c908a980a
NikDoukellis <nik@droplit.io>
2015-06-09 19:53:33 -0400

    Modify discoverer to reuse same ssdp for later search requests.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var WeMo = require('./lib/WeMo');
 
-exports.discover = require('./lib/Discoverer');
+exports.discoverer = require('./lib/Discoverer');
 
 exports.createClient = function(config) {
   return new WeMo(config);
